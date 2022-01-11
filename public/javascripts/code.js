6b19f9fd052466260141be2d3c78679cadf85a54
clement-massit <clement.massit@gmail.com>
2022-01-11 12:03:43 +0100

    Update code.js

--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -43,6 +43,62 @@ promises.push(d3.json('./data/Mass_shooting_data.json'))
 
 var states_data = {};
 
+all_states = {
+    "Alabama"                :  "01",
+    "Alaska"                 :  "02",
+    "Arizona"                :  "04",
+    "Arkansas"               :  "05",
+    "California"             :  "06",
+    "Colorado"               :  "08",
+    "Connecticut"            :  "09",
+    "Delaware"               :  "10",
+    "District of Columbia"   :  "11",
+    "Florida"                :  "12",
+    "Georgia"                :  "13",
+    "Hawaii"                 :  "15",
+    "Idaho"                  :  "16",
+    "Illinois"               :  "17",
+    "Indiana"                :  "18",
+    "Iowa"                   :  "19",
+    "Kansas"                 :  "20",
+    "Kentucky"               :  "21",
+    "Louisiana"              :  "22",
+    "Maine"                  :  "23",
+    "Maryland"               :  "24",
+    "Massachusetts"          :  "25",
+    "Michigan"               :  "26",
+    "Minnesota"              :  "27",
+    "Mississippi"            :  "28",
+    "Missouri"               :  "29",
+    "Montana"                :  "30",
+    "Nebraska"               :  "31",
+    "Nevada"                 :  "32",
+    "New Hampshire"          :  "33",
+    "New Jersey"             :  "34",
+    "New Mexico"             :  "35",
+    "New York"               :  "36",
+    "North Carolina"         :  "37",
+    "North Dakota"           :  "38",
+    "Ohio"                   :  "39",
+    "Oklahoma"               :  "40",
+    "Oregon"                 :  "41",
+    "Pennsylvania"           :  "42",
+    "Rhode Island"           :  "44",
+    "South Carolina"         :  "45",
+    "South Dakota"           :  "46",
+    "Tennessee"              :  "47",
+    "Texas"                  :  "48",
+    "Utah"                   :  "49",
+    "Vermont"                :  "50",
+    "Virginia"               :  "51",
+    "Washington"             :  "53",
+    "West Virginia"          :  "54",
+    "Wisconsin"              :  "55",
+    "Wyoming"                :  "56",
+    "Puerto Rico"            :  "72",
+    "Virgin Islands"         :  "78"
+  }
+
 Promise.all(promises).then(function (values) {
     let us = values[0]
     let data = values[1]
@@ -52,7 +108,7 @@ Promise.all(promises).then(function (values) {
         
         if (!states_data.hasOwnProperty(data[i]["State"])) {
             state = {};
-            state["id"] = cpt;
+            state["id"] = all_states[data[i]["State"]];
             state["kill"] = 0;
             states_data[data[i]["State"]] = state;
 
@@ -63,7 +119,6 @@ Promise.all(promises).then(function (values) {
 
     }
     
-    Object.keys(states_data).sort()
 
     states = [];
     kills = []
@@ -72,7 +127,7 @@ Promise.all(promises).then(function (values) {
         states.push(state);
         kills.push(values["kill"])
     }
-    
+
     data2 = []
  
     for(let i in states_data){
