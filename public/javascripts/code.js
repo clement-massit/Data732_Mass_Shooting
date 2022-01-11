0e757edbe9294eb4906996abfd0d2420624a7b50
clement-massit <clement.massit@gmail.com>
2022-01-11 11:55:31 +0100

    carte et bar chart

--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -7,10 +7,10 @@ width_map = width_map - margin_map.left - margin_map.right;
 let ratio_map = 0.5;
 let height_map = width_map * ratio_map;
 
-let currentId = 0;
 
 let svgMap = d3.select('body').append('svg')
-    .attr('class', 'center-container')
+    .attr("id", "svg")
+    .attr('class', 'center-container Blues')
     .attr('height', height_map + margin_map.top + margin_map.bottom)
     .attr('width', width_map + margin_map.left + margin_map.right)
     .attr('x', 0)
@@ -27,48 +27,30 @@ let projection_map = d3.geoAlbersUsa()
 
 let path_map = d3.geoPath().projection(projection_map);
 
-let g = svgMap.append("g")
+let gmap = svgMap.append("g")
     .attr('class', 'center-container center-items us-state')
     .attr('transform', 'translate(' + margin_map.left + ',' + margin_map.top + ')')
     .attr('width', width_map + margin_map.left + margin_map.right)
     .attr('height', height_map + margin_map.top + margin_map.bottom);
 
-Promise.resolve(d3.json('javascripts/us.json')).then(function (us) {
-    g.append("g")
-        .attr("id", "counties")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-        .enter().append("path")
-        .attr("d", path_map)
-        .attr("class", "county-boundary")
-        .on('mouseover', function (d) {
-            tableFirstId = 0;
-            displayStateDeath(currentId);
-            //drawPieCharts(getStateDataForPie(currentId));
-            //setViewLabel(currentId);
-            //showTable(currentId);
-            //showBarChart(currentId);
-            console.log(currentId);
-        });
-});
+var div = d3.select("body").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0);
+///////////////////// Création de la carte ////////////////////////////
+promises = []
+promises.push(d3.json('javascripts/us.json'))
+promises.push(d3.json('./data/Mass_shooting_data.json'))
 
-function displayStateDeath(currentId) {
+var states_data = {};
 
-}
-
-
-
-var datas;
-var total_death = 0;
-var states_data = new Map();
-var cpt = 0
-
-
-d3.json("./data/Mass_shooting_data.json").then(function (data) {
-    datas = data
+Promise.all(promises).then(function (values) {
+    let us = values[0]
+    let data = values[1]
+    cpt = 0
     for (var i = 0; i < data.length; i++) {
         total_death += parseInt(data[i]["# Killed"]);
-        if (!states_data.has(data[i]["State"])) {
+        
+        if (!states_data.hasOwnProperty(data[i]["State"])) {
             state = {};
             state["id"] = cpt;
             state["kill"] = 0;
@@ -76,84 +58,239 @@ d3.json("./data/Mass_shooting_data.json").then(function (data) {
 
             cpt++;
         }
+        
         states_data[data[i]["State"]]["kill"] += data[i]["# Killed"];
 
-        /*
-                console.log(data[i]["Incident ID"]);
-                console.log(data[i]["Incident Date"]);
-                console.log(data[i]["State"]);
-                console.log(data[i]["City Or County"]);
-                console.log(data[i]["Address"]);
-                console.log(data[i]["# Killed"]);
-                console.log(data[i]["# Injured"]);
-                console.log(data[i]["Operations"]);*/
     }
+    
+    Object.keys(states_data).sort()
 
+    states = [];
+    kills = []
+    
+    for(const [state, values] of Object.entries(states_data)){
+        states.push(state);
+        kills.push(values["kill"])
+    }
+    
+    data2 = []
+ 
+    for(let i in states_data){
+    
+        tmp = {}
+        tmp["id"]  = states_data[i]["id"]
+        tmp["State"] = i
+        tmp["# Killed"] = states_data[i]["kill"]
+        data2.push(tmp)
+    }
+    
+    
+    gmap.append("g")
+        .attr("id", "counties")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+        
+        .attr("d", path_map)
+        .attr('id', d => "d" + d["id"])
+        .attr("class", "county-boundary")
+        
+    
+/*
+    gmap.append("g")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+        .attr('id', d => "d" + d["id"])
+        .attr("d", path_map);
+*/
+    var quantile = d3.scaleQuantile()
+        .domain([0, d3.max(data, e => +e["# Killed"])])
+        .range(d3.range(9));
+    
+    var legend = svgMap.append('g')
+        .attr('transform', 'translate(850, 150)')
+        .attr('id', 'legend');
+        
+    legend.selectAll('.colorbar')
+        .data(d3.range(9))
+        .enter().append('svg:rect')
+            .attr('y', d => d * 20 + 'px')
+            .attr('height', '20px')
+            .attr('width', '20px')
+            .attr('x', '0px')
+            .attr("class", d => "Blues q" + d + "-9")
+
+    var legendScale = d3.scaleLinear()
+        .domain([0, d3.max(data, e => +e["# Killed"])])
+        .range([0, 9 * 20]);
+    
+    var legendAxis = svg.append("g")
+        .attr('transform', 'translate(550, -100)')
+        .call(d3.axisRight(legendScale).ticks(6));
+    console.log(data2)
+    data2.forEach(function(e,i) {
+        d3.select("#d" + e["id"])     
+            .attr("class", d =>"state q" + quantile(+e["# Killed"]) + "+9")   
+            .on("mouseover", function(event, d) {
+                console.log(e)
+                div.transition()        
+                    .duration(200)      
+                    .style("opacity", .9);
+                div.html("<b>State : </b>" + data2[i]["State"] + "<br>"
+                        + "<b>Kills : </b>" + data2[i]["# Killed"] + "<br>")
+                    .style("left", (event.pageX + 30) + "px")     
+                    .style("top", (event.pageY - 30) + "px");
+                    
+            })
+            .on("mouseout", function(event, d) {
+                    div.style("opacity", 0);
+                    div.html("")
+                        .style("left", "-500px")
+                        .style("top", "-500px");
+            });
+    });
+    d3.select("select").on("change", function() {
+        d3.selectAll("svg").attr("class", this.value);
+    });
 });
 
-console.log(states_data);
-console.log(total_death);
+function displayStateDeath(currentId) {
+    console.log(currentId)
+
+}
 
 
-const margin_barchart = { top: 20, right: 20, bottom: 90, left: 120 },
-    width_barchart = 800 - margin_barchart.left - margin_barchart.right,
-    height_barchart = 400 - margin_barchart.top - margin_barchart.bottom;
 
-const x_barchart = d3.scaleBand()
-    .range([0, width_barchart])
+
+
+
+
+//////////////////////////////////////////////////////////////////BAR CHART//////////////////////////////////////////////////////////////////
+
+const margin = {top: 20, right: 20, bottom: 90, left: 120},
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+const x = d3.scaleBand()
+    .range([0, width])
     .padding(0.1);
 
-const y_barchart = d3.scaleLinear()
-    .range([height_barchart, 0]);
+const y = d3.scaleLinear()
+    .range([height, 0]);
 
-const svg_barchart = d3.select("body").append("svg_barchart")
+const svg = d3.select("body").append("svg")
     .attr("id", "svg")
-    .attr("width", width_barchart + margin_barchart.left + margin_barchart.right)
-    .attr("height", height_barchart + margin_barchart.top + margin_barchart.bottom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin_barchart.left + "," + margin_barchart.top + ")");
-
-const div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 1);
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+let total_death = 0;
+states_data = {};
+var cpt = 0
 
-states_data.forEach(state => {
+d3.json("./data/Mass_shooting_data.json").then(function(data) {
+    ///////////////////// Initialisation des données ////////////////////////////
+    for (var i = 0; i < data.length; i++) {
+        total_death += parseInt(data[i]["# Killed"]);
+        
+        if (!states_data.hasOwnProperty(data[i]["State"])) {
+            state = {};
+            state["id"] = cpt;
+            state["kill"] = 0;
+            states_data[data[i]["State"]] = state;
+
+            cpt++;
+        }
+        
+        states_data[data[i]["State"]]["kill"] += data[i]["# Killed"];
+
+    }
+    
+    states = [];
+    kills = []
+    
+    for(const [state, values] of Object.entries(states_data)){
+        states.push(state);
+        kills.push(values["kill"])
+    }
+    
+
+
+    ///////////////////// Création du barchart ////////////////////////////
+
+    data2 = []
+    for(let i in states){
+        tmp = {}
+        tmp["State"] = states[i]
+        tmp["# Killed"] = kills[i]
+        data2.push(tmp)
+    }
+
+    data2.forEach(d => d["# Killed"] = +d["# Killed"]);
 
     // Mise en relation du scale avec les données de notre fichier
     // Pour l'axe X, c'est la liste des pays
     // Pour l'axe Y, c'est le max des populations
-    x.domain(state.getKey());
-    y.domain(state["kill"]);
-
+    //console.log(data.map(d => d["State"]))
+    x.domain(data2.map(d => d["State"]));
+    y.domain([0, d3.max(kills)]);
+    
+    
     // Ajout de l'axe X au SVG
     // Déplacement de l'axe horizontal et du futur texte (via la fonction translate) au bas du SVG
     // Selection des noeuds text, positionnement puis rotation
-    svg_barchart.append("g")
-        .attr("transform", "translate(0," + height_barchart + ")")
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x).tickSize(0))
         .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)");
-
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)");
     // Ajout de l'axe Y au SVG avec 6 éléments de légende en utilisant la fonction ticks (sinon D3JS en place autant qu'il peut).
-    svg_barchart.append("g")
+    svg.append("g")
         .call(d3.axisLeft(y).ticks(6));
 
     // Ajout des bars en utilisant les données de notre fichier data.tsv
     // La largeur de la barre est déterminée par la fonction x
     // La hauteur par la fonction y en tenant compte de la population
     // La gestion des events de la souris pour le popup
-    svg_barchart.selectAll(".bar")
-        .data(states_data)
+  
+    
+    
+
+    svg.selectAll(".bar")
+        .data(data2)
         .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", x(state.getKey()))
-        .attr("width", x.bandwidth())
-        .attr("y", y(state["kill"]))
-        .attr("height", height_barchart - y(state["kill"]))
-        
+            .attr("class", "bar")
+            .attr("x", d => x(d["State"]))
+            .attr("width", x.bandwidth())
+            .attr("y", d => y(d["# Killed"]))
+            .attr("height", d => height - y(d["# Killed"]))
+            .on("mouseover", function(event, d) {
+                div.transition()        
+                    .duration(200)      
+                    .style("opacity", .9);
+                
+                div.html("State : " + data2[d]["State"] + "<br>Kills : " + data2[d]["# Killed"])
+                    .style("left", (event.pageX + 10) + "px")     
+                    .style("top", (event.pageY - 50) + "px");
+            })
+            .on("mouseout", function(event, d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+    
+
 });
+
+
+
+
+
+
+    
